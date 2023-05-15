4bfc544e4405ac1355223d2c328af660a1e7206f
Mohamad Aid Bacuk Zanab <mohammedsharif067@gmail.com>
2023-05-16 01:56:24 +0300

    Done with the Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+// A divide function 
+const divide = (a, b) => a / b;
+divide(2000,100);
+
+
+// square arrow function
+const square = (c) => c ** 2;
+
+// An arrow function called add
+const add = (a, b) => a + b;
